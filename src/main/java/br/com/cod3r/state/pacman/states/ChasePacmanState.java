cb8e06e13afac94e7373a80ce1e2aded1c1c7187
Marcelo Sant'Anna <marcelojunin2010@hotmail.com>
2022-10-03 21:44:54 -0300

    State implementation

--- src/main/java/br/com/cod3r/state/pacman/states/ChasePacmanState.java
+++ src/main/java/br/com/cod3r/state/pacman/states/ChasePacmanState.java
@@ -2,23 +2,23 @@ package br.com.cod3r.state.pacman.states;
 
 import br.com.cod3r.state.pacman.Ghost;
 
-public class WanderMazeState implements GhostState {
-	private static final WanderMazeState instance = new WanderMazeState();
-	private WanderMazeState() {};
+public class ChasePacmanState implements GhostState {
+	private static final ChasePacmanState instance = new ChasePacmanState();
+	private ChasePacmanState() {};
 	
-	public static WanderMazeState getInstance() {
+	public static ChasePacmanState getInstance() {
 		return instance;
 	}
 
 	@Override
 	public void spotPacman(Ghost ghost) {
-		System.out.println("I've just spot the pacman, let's chase him!");
-		ghost.setState(ChasePacmanState.getInstance());
+		System.out.println("I know, I'm already chasing him...");
 	}
 
 	@Override
 	public void losePacman(Ghost ghost) {
-		System.out.println("How to lose what I haven't found?");
+		System.out.println("The Pacman ran away, let's find him again");
+		ghost.setState(WanderMazeState.getInstance());
 	}
 
 	@Override
